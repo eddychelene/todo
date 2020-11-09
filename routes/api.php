ce5a87ec3543435a5b142217380deaacb377d555
eddychelene <edmilsonchelene20@gmail.com>
2020-11-09 17:57:45 +0200

    add model store

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("tasks", "App\Http\Controllers\TasksController@index");
+Route::post("tasks", "App\Http\Controllers\TasksController@store");
